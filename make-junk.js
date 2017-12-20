7a89a8585b34a4bfeb3623b9a6be20bac942e3ad
dustinpfister <dustin.pfister@gmail.com>
2017-12-20 16:53:45 -0500

    mkjunk

--- make-junk.js
+++ make-junk.js
@@ -1,15 +1,33 @@
 var mkFiles = require('./mkfiles.js');
 
-mkFiles.mkFiles();
+// source/
 mkFiles.mkFiles({
 
-    p: './source/css',
-    type: '.css'
+    p: './source',
+    type: '.txt'
 
+});
+
+// source/html
+mkFiles.mkFiles({
+    p: './source/html',
+    type: '.txt'
 });
 mkFiles.mkFiles({
 
     p: './source/html',
     type: '.html'
+
+});
+
+// source/html/css
+mkFiles.mkFiles({
+    p: './source/html/css',
+    type: '.txt'
+});
+mkFiles.mkFiles({
+
+    p: './source/html/css',
+    type: '.css'
 
 });
